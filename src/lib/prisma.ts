734e60e8a4b694a59b06235b87c71282a71e4294
fakhreddine.benfraj <fakhreddine.benfraj@jiway.lu>
2026-03-05 14:51:28 +0100

    added postinstall prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,14 +10,14 @@ const globalForPrisma = globalThis as unknown as {
 
 function createPrismaClient() {
   const connectionString = process.env.DATABASE_URL;
-  
+
   if (!connectionString) {
     throw new Error("DATABASE_URL environment variable is not set");
   }
-  
+
   const pool = new Pool({ connectionString });
   const adapter = new PrismaPg(pool);
-  
+
   return new PrismaClient({ adapter });
 }
 
